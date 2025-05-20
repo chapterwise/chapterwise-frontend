409c7575c820b930f8e632c42f669c9dc3348099
yakatyansh <ykpetrolhead08@outlook.com>
2025-05-20 23:37:42 +0530

    faq support skeleton code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Header from "./components/header";
 import MissionPage from "./pages/about";
+import HelpCenter from "./pages/faqsupport";
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
       </header>
       <main className="flex min-h-screen flex-col items-center justify-center p-0 pt-24 w-full">
         <div className="w-full h-full px-4">
-          <MissionPage />
+          <HelpCenter />
         </div>
       </main>
     </>
